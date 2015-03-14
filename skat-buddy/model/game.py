d53c5175142962191f222359b63cac7bd7794fc7
dkiefner <pivi_dev@gmx.de>
2015-03-14 23:48:36 +0100

    fix wrong give out type

--- skat-buddy/model/game.py
+++ skat-buddy/model/game.py
@@ -42,17 +42,17 @@ class Game:
     # give cards P1:3, P2:3, P3:3, S:2, P1:4, P2:4, P3:4, P1:3, P2:3, P3:3 --> skat alike
     def give_out_cards(self):
         # round 1
-        self.players[0].cards.append(self.card_deck[:3])
-        self.players[1].cards.append(self.card_deck[3:6])
-        self.players[2].cards.append(self.card_deck[6:9])
-        self.skat.append(self.card_deck[9:11])
+        self.players[0].cards.extend(self.card_deck[:3])
+        self.players[1].cards.extend(self.card_deck[3:6])
+        self.players[2].cards.extend(self.card_deck[6:9])
+        self.skat.extend(self.card_deck[9:11])
 
         # round 2
-        self.players[0].cards.append(self.card_deck[11:15])
-        self.players[1].cards.append(self.card_deck[15:19])
-        self.players[2].cards.append(self.card_deck[19:23])
+        self.players[0].cards.extend(self.card_deck[11:15])
+        self.players[1].cards.extend(self.card_deck[15:19])
+        self.players[2].cards.extend(self.card_deck[19:23])
 
         # round 3
-        self.players[0].cards.append(self.card_deck[23:26])
-        self.players[1].cards.append(self.card_deck[26:29])
-        self.players[2].cards.append(self.card_deck[29:32])
+        self.players[0].cards.extend(self.card_deck[23:26])
+        self.players[1].cards.extend(self.card_deck[26:29])
+        self.players[2].cards.extend(self.card_deck[29:32])
